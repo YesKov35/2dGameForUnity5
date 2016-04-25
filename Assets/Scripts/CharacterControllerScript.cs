a0bad1d8706b3dc9b327f59f21c06a91bf196ce5
NikolayYeskov <lnik35p@gmail.com>
2016-04-25 15:29:45 +0300

    add project

---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CharacterControllerScript : NetworkBehaviour
+{
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+    public Rigidbody2D Body;
+
+    Animator anim;
+    public Camera cam;
+
+    // Use this for initialization
+    void Start ()
+    {
+        cam = transform.GetComponentInChildren<Camera>().GetComponent<Camera>();
+        anim = GetComponent<Animator>();
+        Body = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (isLocalPlayer)
+        {
+            float move = Input.GetAxis("Horizontal");
+
+            anim.SetFloat("Speed", Mathf.Abs(move));
+
+            Body.velocity = new Vector2(move * maxSpeed, Body.velocity.y);
+
+            if (move > 0 && !facingRight)
+                Flip();
+            else if (move < 0 && facingRight)
+                Flip();
+        }
+        else
+        {
+            if (cam.enabled)
+            {
+                cam.enabled = false;
+                cam.gameObject.GetComponent<AudioListener>().enabled = false;
+            }
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale; 
+    }
+}
---
+++ Assets/Scripts/PlayerUpdate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    public Camera cam;
+    // Use this for initialization
+    void Start()
+    {
+        cam = transform.GetComponentInChildren<Camera>().GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+
+        }
+        else
+        {
+            if (cam.enabled)
+            {
+                cam.enabled = false;
+                cam.gameObject.GetComponent<AudioListener>().enabled = false;
+            }
+        }
+    }
+}
